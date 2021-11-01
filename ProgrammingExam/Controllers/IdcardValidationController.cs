59d4045fa117951c5084d40c341dc37b7fa99a45
Potsawat Jaikwang <potsawat.jai@gmail.com>
2021-11-01 09:50:46 +0700

    cmmt add check regex

--- ProgrammingExam/Controllers/IdcardValidationController.cs
+++ ProgrammingExam/Controllers/IdcardValidationController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,38 +24,47 @@ namespace ProgrammingExam.Controllers
             bool chkcitizen = true;
             var i = 0;
             var total = 0;
-
+            Match matchint = Regex.Match(id.citizenid, "^[0-9]+$");
             if (id.citizenid.Length != 13 || id.citizenid.Length == 0) chkcitizen = false;
-
-            if (chkcitizen)
+            if (matchint.Success)
             {
-                string[] idcard = id.citizenid.ToCharArray().Select(c => c.ToString()).ToArray();
-                foreach (var s in idcard)
-                {
-                    i++;
-                    var value = Convert.ToInt32(s);
-                    //sum += Convert.ToInt32(s) * (13 - i);
-                    total = (total + ((value) * (13 - i)));
-
-                }
-                if ((11 - total % 11) % 10 != float.Parse(id.citizenid.Substring(12))) chkcitizen = false; chkcitizen = true;
-
                 if (chkcitizen)
                 {
-                    reply.success = true;
-                    reply.error_code = "200";
-                    reply.error_msg = "";
-                } else
+                    string[] idcard = id.citizenid.ToCharArray().Select(c => c.ToString()).ToArray();
+                    foreach (var s in idcard)
+                    {
+                        i++;
+                        var value = Convert.ToInt32(s);
+                        //sum += Convert.ToInt32(s) * (13 - i);
+                        total = (total + ((value) * (13 - i)));
+
+                    }
+                    if ((11 - total % 11) % 10 != float.Parse(id.citizenid.Substring(12))) chkcitizen = false; chkcitizen = true;
+
+                    if (chkcitizen)
+                    {
+                        reply.success = true;
+                        reply.error_code = "200";
+                        reply.error_msg = "";
+                    }
+                    else
+                    {
+                        reply.success = false;
+                        reply.error_code = "001";
+                        reply.error_msg = "citizen_id invalid.";
+                    }
+                }
+                else
                 {
                     reply.success = false;
                     reply.error_code = "001";
-                    reply.error_msg = "citizen_id invalid.";
+                    reply.error_msg = "citizen_id is require.";
                 }
             } else
             {
                 reply.success = false;
                 reply.error_code = "001";
-                reply.error_msg = "citizen_id is require.";
+                reply.error_msg = "citizen_id invalid.";
             }
 
             return reply;
